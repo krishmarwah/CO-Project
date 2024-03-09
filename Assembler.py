39e0fe1af5cd0f4bfbc1b33b9dbabfccf2cd8468
akshatverma069 <147979467+akshatverma069@users.noreply.github.com>
2024-03-09 23:54:55 +0530

    Update Assembler.py

--- Assembler.py
+++ Assembler.py
@@ -152,91 +152,97 @@ line="add s1,s2,s3"
 input_lines=[line]
 output_lines=[]
 
+labels = {}
+current_address=0
+for i in range(len(input_lines)):
+    line = input_lines[i].strip("\n")
+    if line[-1]==(':'):
+        label = line[:-1]
+        if label in labels:
+            errors.append("Error: Duplicate label '" + label + "' on line " + str(i+1))
+        else:
+            labels[label] = current_address
+    else:
+        current_address += 4
 def find_errors(input_lines):
     errors = []
-    labels = {}
-    current_address = 0
-    for i in range(len(input_lines)):
-        line = input_lines[i].strip("\n")
-        if line[-1]==(':'):
-            label = line[:-1]
-            if label in labels:
-                errors.append("Error: Duplicate label '" + label + "' on line " + str(i+1))
-            else:
-                labels[label] = current_address
+    div = line.split(" ")
+    if div[0] in R:
+        k = div[1].split(',')
+        if div[0] not in opcode.keys():
+            errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
+        elif k[0] not in regs.keys():
+            errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
+        elif k[1] not in regs.keys():
+            errors.append("Error: Invalid register '" + k[1] + "' on line " + str(i+1))
+        elif k[2] not in regs.keys():
+            errors.append("Error: Invalid register '" + k[2] + "' on line " + str(i+1))
+    elif div[0] in I:
+        k = div[1].split(',')
+        if div[0] != 'lw':
+            if div[0] not in opcode.keys():
+                errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
+            elif k[0] not in regs.keys():
+                errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
+            elif k[1] not in regs.keys():
+                errors.append("Error: Invalid register '" + k[1] + "' on line " + str(i+1))
+            elif int(k[2]) < -2**11 or int(k[2]) >= 2**11:
+                errors.append("Error: Immediate value " + k[2] + " out of range on line " + str(i+1))
         else:
-            div = line.split(" ")
-            current_address += 4
-            if div[0] in R:
-                k = div[1].split(',')
-                if div[0] not in opcode.keys():
-                    errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
-                elif k[0] not in regs.keys():
-                    errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
-                elif k[1] not in regs.keys():
-                    errors.append("Error: Invalid register '" + k[1] + "' on line " + str(i+1))
-                elif k[2] not in regs.keys():
-                    errors.append("Error: Invalid register '" + k[2] + "' on line " + str(i+1))
-            elif div[0] in I:
-                k = div[1].split(',')
-                if div[0] != 'lw':
-                    if div[0] not in opcode.keys():
-                        errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
-                    elif k[0] not in regs.keys():
-                        errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
-                    elif k[1] not in regs.keys():
-                        errors.append("Error: Invalid register '" + k[1] + "' on line " + str(i+1))
-                    elif int(k[2]) < -2**11 or int(k[2]) >= 2**11:
-                        errors.append("Error: Immediate value " + k[2] + " out of range on line " + str(i+1))
-                else:
-                    d = line.split(",")
-                    op = d[0].split()[0]
-                    rs2 = d[0].split()[1]
-                    rest = d[1].split("(")
-                    rs1 = rest[1].split(")")[0]
-                    imm = rest[0]
-                    if d[0] not in opcode.keys():
-                        errors.append("Error: Invalid instruction '" + d[0] + "' on line " + str(i+1))
-                    elif rs2 not in regs.keys():
-                        errors.append("Error: Invalid register '" + rs2 + "' on line " + str(i+1))
-                    elif rs1 not in regs.keys():
-                        errors.append("Error: Invalid register '" + rs1 + "' on line " + str(i+1))
-                    elif int(imm) < -2**11 or int(imm) >= 2**11:
-                        errors.append("Error: Immediate value " + imm + " out of range on line " + str(i+1))
-            elif div[0] in S:
-                d = line.split(",")
-                op = d[0].split()[0]
-                rs2 = d[0].split()[1]
-                rest = d[1].split("(")
-                rs1 = rest[1].split(")")[0]
-                imm = rest[0]
-                if d[0] not in opcode.keys():
-                    errors.append("Error: Invalid instruction '" + d[0] + "' on line " + str(i+1))
-                elif rs2 not in regs.keys():
-                    errors.append("Error: Invalid register '" + rs2 + "' on line " + str(i+1))
-                elif rs1 not in regs.keys():
-                    errors.append("Error: Invalid register '" + rs1 + "' on line " + str(i+1))
-                elif int(imm) < -2**11 or int(imm) >= 2**11:
-                    errors.append("Error: Immediate value " + imm + " out of range on line " + str(i+1))
-            elif div[0] in B:
-                k = div[1].split(',')
-                if div[0] not in opcode.keys():
-                    errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
-                elif k[0] not in regs.keys():
-                    errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
-                elif k[1] not in regs.keys():
-                    errors.append("Error: Invalid register '" + k[1] + "' on line " + str(i+1))
-                elif int(k[2]) < -2**11 or int(k[2]) >= 2**11:
-                    errors.append("Error: Immediate value " + k[2] + " out of range on line " + str(i+1))
-            elif div[0] in U or div[0] in J:
-                k = div[1].split(',')
-                if div[0] not in opcode.keys():
-                    errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
-                elif k[0] not in regs.keys():
-                    errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
-                elif int(k[1]) < -2**19 or int(k[1]) >= 2**19:
-                    errors.append("Error: Immediate value " + k[1] + " out of range on line " + str(i+1))
-    return errors, labels
+            d = line.split(",")
+            op = d[0].split()[0]
+            rs2 = d[0].split()[1]
+            rest = d[1].split("(")
+            rs1 = rest[1].split(")")[0]
+            imm = rest[0]
+            if d[0] not in opcode.keys():
+                errors.append("Error: Invalid instruction '" + d[0] + "' on line " + str(i+1))
+            elif rs2 not in regs.keys():
+                errors.append("Error: Invalid register '" + rs2 + "' on line " + str(i+1))
+            elif rs1 not in regs.keys():
+                errors.append("Error: Invalid register '" + rs1 + "' on line " + str(i+1))
+            elif int(imm) < -2**11 or int(imm) >= 2**11:
+                errors.append("Error: Immediate value " + imm + " out of range on line " + str(i+1))
+    elif div[0] in S:
+        d = line.split(",")
+        op = d[0].split()[0]
+        rs2 = d[0].split()[1]
+        rest = d[1].split("(")
+        rs1 = rest[1].split(")")[0]
+        imm = rest[0]
+        if d[0] not in opcode.keys():
+            errors.append("Error: Invalid instruction '" + d[0] + "' on line " + str(i+1))
+        elif rs2 not in regs.keys():
+            errors.append("Error: Invalid register '" + rs2 + "' on line " + str(i+1))
+        elif rs1 not in regs.keys():
+            errors.append("Error: Invalid register '" + rs1 + "' on line " + str(i+1))
+        elif int(imm) < -2**11 or int(imm) >= 2**11:
+            errors.append("Error: Immediate value " + imm + " out of range on line " + str(i+1))
+    elif div[0] in B:
+        k = div[1].split(',')
+        if div[0] not in opcode.keys():
+            errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
+        elif k[0] not in regs.keys():
+            errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
+        elif k[1] not in regs.keys():
+            errors.append("Error: Invalid register '" + k[1] + "' on line " + str(i+1))
+        elif k[2].isdigit(): 
+            imm = int(k[2])
+            if imm < -2048 or imm >= 2048:
+                errors.append("Error: Immediate value " + str(imm) + " out of range on line " + str(i+1))
+        elif k[2] not in labels:  
+            errors.append("Error: Label '" + k[2] + "' not found on line " + str(i+1))
+
+    elif div[0] in U or div[0] in J:
+        k = div[1].split(',')
+        if div[0] not in opcode.keys():
+            errors.append("Error: Invalid instruction '" + div[0] + "' on line " + str(i+1))
+        elif k[0] not in regs.keys():
+            errors.append("Error: Invalid register '" + k[0] + "' on line " + str(i+1))
+        elif int(k[1]) < -2**19 or int(k[1]) >= 2**19:
+            errors.append("Error: Immediate value " + k[1] + " out of range on line " + str(i+1))
+    return errors
+
 
 c=0
 errors = find_errors(input_lines)
