661a21595fda8626a46217f664ee775dcf549b99
krishmarwah <141606907+krishmarwah@users.noreply.github.com>
2024-04-17 12:10:19 +0530

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -25,10 +25,10 @@ def twos_complement(binary_str):
         return int(binary_str, 2)
 
 
-def simulator(s):
-    global PC
+def simulator(s,PC):
     #R-Type
     #add
+    ISB = False
     if (s[25:32]=='0110011' and s[17:20]=='000' and s[0:7]=='0000000'):
         regs[s[20:25]]=regs[s[7:12]]+regs[s[12:17]]
     #sub    
@@ -69,13 +69,13 @@ def simulator(s):
         regs[s[20:25]]=regs[s[7:12]] & regs[s[12:17]]   
     #I-Type
     #lw
-    elif s[-7:]=="0000011" and s[-14:-11]=="010":
-        dest = regs[s[-12:-7]]
+    elif s[-7:]=="0000011" and s[-15:-12]=="010":
+        dest = s[-12:-7]
         op1 = regs[s[-20:-15]]
         imm = s[0:12]
-        regs[dest] = memory_locations[address_specifier[(regs[op1] + int(imm,2) - 65536)//4]]
+        regs[dest] = memory_locations[address_specifier[(op1 + int(imm,2) - 65536)//4]]
    #addi
-    elif s[-7:]=="0010011" and s[-15:-12:-1]=="000":
+    elif s[-7:]=="0010011" and s[-15:-12]=="000":
         rd=s[20:25]
         op1=s[12:17]
         imm=s[0:12]
@@ -83,78 +83,86 @@ def simulator(s):
         
         
     #sltiu
-    elif s[-7:]=="0010011" and s[-15:-12:-1]=="011":
+    elif s[-7:]=="0010011" and s[-15:-12]=="011":
         rd=s[20:25]
         op1=s[12:17]
         imm=s[0:12]
         if regs[op1]<int(imm,2):
             regs[rd]=1
     #jalr
-    elif s[-7:]=="1100111" and s[-14:-11]=="000":
+    elif s[-7:]=="1100111" and s[-15:-12]=="000":
         dest = regs[s[-12:-7]]
         imm = int(s[0:12],2)
         op1 = regs[s[12:17]]
         regs[dest] = PC + 4
         PC = regs[op1] + imm   
         PC = PC&0xFFFFFFFE
+        ISB=True
     #S-Type
     #sw
-    elif s[-7:]=="0100011" and s[-15:-12:-1]=="010":
+    elif s[-7:]=="0100011" and s[-15:-12]=="010":
         imm = int(s[0:7]+s[20:25],2)
+        print (imm)
         rs1 = regs[s[12:17]]
-        memory_int = regs[rs1] + imm
+        memory_int = rs1 + imm
         rs2 = regs[s[7:12]]
-        memory_locations[address_specifier[(memory_int-65536)//4]] = regs[rs2]
+        memory_locations[address_specifier[(memory_int-65536)//4]] = rs2
 
     #B-Type
     #beq
-    elif s[-7:]=="1100011" and s[-15:-12:-1]=="000":
+    elif s[-7:]=="1100011" and s[-15:-12]=="000":
         rs1=s[12:17]
         rs2=s[7:12]
         imm=int(s[:7]+s[20:25])
         if(regs[rs1]==regs[rs2]):
+            ISB=True
             PC+=imm
     #bne
-    elif s[-7:]=="1100011" and s[-15:-12:-1]=="001":
+    elif s[-7:]=="1100011" and s[-15:-12]=="001":
         rs1=s[12:17]
         rs2=s[7:12]
         imm=int(s[:7]+s[20:25])
         if(regs[rs1]!=regs[rs2]):
+            ISB=True
             PC+=imm
         
 
     #blt
-    elif s[-7:]=="1100011" and s[-15:-12:-1]=="100":
+    elif s[-7:]=="1100011" and s[-15:-12]=="100":
         rs1=s[12:17]
         rs2=s[7:12]
         imm=int(s[:7]+s[20:25])
         if(regs[rs1]<regs[rs2]):
+            ISB=True
             PC+=imm
         
 
     #bge
-    elif s[-7:]=="1100011" and s[-15:-12:-1]=="101":
+    elif s[-7:]=="1100011" and s[-15:-12]=="101":
         rs1=s[12:17]
         rs2=s[7:12]
         imm=int(s[:7]+s[20:25])
         if(regs[rs1]>=regs[rs2]):
+            ISB=True
             PC+=imm
 
     #bltu
-    elif s[-7:]=="1100011" and s[-15:-12:-1]=="110":
+    elif s[-7:]=="1100011" and s[-15:-12]=="110":
         rs1=s[12:17]
         rs2=s[7:12]
         imm=int(s[:7]+s[20:25])
         if(regs[rs1]<regs[rs2]):
+            ISB=True
             PC+=imm
         
     
     #bgeu
-    elif s[-7:]=="1100011" and s[-15:-12:-1]=="111":
+    elif s[-7:]=="1100011" and s[-15:-12]=="111":
         rs1=s[12:17]
         rs2=s[7:12]
         imm=int(s[:7]+s[20:25])
         if(regs[rs1]>=regs[rs2]):
+            ISB=True
             PC+=imm
 
     #U-Type
@@ -175,13 +183,20 @@ def simulator(s):
     elif s[-7:]=="1101111":
         rd=s[20:25]
         imm=int(s[:20])
-    
+        ISB=True
+    if not ISB:
+        PC+=4
 
 
-
-
-program=open("program.txt","r")
-regs={"00000": 0, "00001": 0, "00010": 0, "00011": 0,
+PC=0
+program=open("program.txt",'r')
+instructions={}
+for i in program:
+    instructions[PC]=i.strip()
+    PC+=4
+PC=0
+print(instructions)
+regs={"00000": 0, "00001": 0, "00010": 256, "00011": 0,
     "00100": 0, "00101": 0, "00110": 0, "00111": 0,
     "01000": 0, "01001": 0, "01010": 0, "01011": 0,
     "01100": 0, "01101": 0, "01110": 0, "01111": 0,
@@ -191,13 +206,28 @@ regs={"00000": 0, "00001": 0, "00010": 0, "00011": 0,
     "11100": 0, "11101": 0, "11110": 0, "11111": 0}
 memory_locations = { "0x00010000" : 0 , "0x00010004" : 0 , "0x00010008" : 0 , "0x0001000c" : 0 , "0x00010010":0 ,"0x00010014":0,"0x00010018":0,"0x0001001c":0,"0x00010020":0,"0x00010024":0,"0x00010028":0,"0x0001002c":0,"0x00010030":0,"0x00010034":0,"0x00010038":0,"0x0001003c":0,"0x00010040":0,"0x00010044":0,"0x00010048":0,"0x0001004c":0,"0x00010050":0,"0x00010054":0,"0x00010058":0,"0x0001005c":0,"0x00010060":0,"0x00010064":0,"0x00010068":0,"0x0001006c":0,"0x00010070":0,"0x00010074":0,"0x00010078":0,"0x0001007c":0}
 address_specifier = {  0 : "0x00010000" , 1 : "0x00010004" ,2:"0x00010008" ,3 : "0x0001000c"  ,4: "0x00010010" ,5:"0x00010014",6:"0x00010018", 7: "0x0001001c",8:"0x00010020",9 :"0x001010024",10:"0x00010028",11:"0x0001002c",12:"0x00010030",13:"0x00010034",14:"0x00010038",15:"0x0001003c",16:"0x00010040",17:"0x00010044",18:"0x00010048",19:"0x0001004c",20:"0x00010050",21:"0x00010054",22:"0x00010058",23:"0x0001005c",24:"0x00010060",25:"0x00010064",26:"0x00010068",27:"0x0001006c",28:"0x00010070",29:"0x00010074",30:"0x00010078",31:"0x0001007c"}
-PC=0
-instructions={}
-for i in program:
-    instructions[PC]=i
-    PC+=4
+count = 0
+outfile = open("output.txt","w")
 for i in instructions:
-    simulator(instructions[i])
+    if instructions[i] == "00000000000000000000000001100011":
+        outfile.write(f"0b{format(i,'032b')} ")
+        for j in regs:
+            outfile.write(f"0b{format(regs[j],'032b')} ")
+        outfile.write("\n")
+        break
+    # m = int(input())
+    simulator(instructions[i],i)
+        # print(count)
+    outfile.write(f"0b{format(i+4,'032b')} ")
+    for j in regs:
+        outfile.write(f"0b{format(regs[j],'032b')} ")
+    outfile.write("\n")
+for i in memory_locations:
+    outfile.write(f"{i}:0b{format(memory_locations[i],'032b')}")
+    outfile.write("\n")
+    print(regs)
+outfile.close()
+print(memory_locations)
 """for i in regs:
     print(f"0b{format(regs[i],'032b')}" , end = " ")"""
 
